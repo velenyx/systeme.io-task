5d173d4f4739f493357d108e57c0fd3c46a68f0f
Sivritkin Dmitriy <dmitriysivritkin@gmail.com>
2024-06-04 19:05:35 +0300

    fix: :ambulance: fix vercel deployment

--- src/shared/api/api.ts
+++ src/shared/api/api.ts
@@ -1,8 +1,10 @@
 import axios from "axios";
 import { ENV } from "~/shared/config/env";
 
+const isVercel = !!ENV.NEXT_PUBLIC_VERCEL_URL;
+
 export const $api = axios.create({
-  baseURL: ENV.NEXT_PUBLIC_API_URL,
+  baseURL: isVercel ? ENV.NEXT_PUBLIC_VERCEL_URL : ENV.NEXT_PUBLIC_API_URL,
   validateStatus(status) {
     return status >= 200 && status < 300;
   },
--- src/shared/config/env.ts
+++ src/shared/config/env.ts
@@ -2,6 +2,7 @@ import { z } from "zod";
 
 export const envSchema = z.object({
   NEXT_PUBLIC_API_URL: z.string(),
+  NEXT_PUBLIC_VERCEL_URL: z.string(),
 });
 
 export type EnvType = z.infer<typeof envSchema>;
