4add7e6f4ff80ee4c86a9548ee84297a02d61ce7
Sivritkin Dmitriy <dmitriysivritkin@gmail.com>
2024-06-04 17:09:21 +0300

    feat: :sparkles: add theme

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Rubik as FontSans } from "next/font/google";
 import "./globals.css";
 import { cn } from "~/shared/lib/utils";
 import { Header } from "~/components/header";
+import { ThemeProvider } from "~/app/providers/theme";
 
 const fontSans = FontSans({ subsets: ["latin"], variable: "--font-sans" });
 
@@ -24,10 +25,17 @@ export default function RootLayout({
           fontSans.variable,
         )}
       >
-        <Header />
-        <main className="flex min-h-screen flex-col items-center justify-between pt-6">
-          {children}
-        </main>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <Header />
+          <main className="flex min-h-screen flex-col items-center justify-between pt-6">
+            {children}
+          </main>
+        </ThemeProvider>
       </body>
     </html>
   );
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,16 +1,27 @@
 import { PanelBottom } from "lucide-react";
 import Link from "next/link";
+import { ThemeToggle } from "~/components/theme-toggle";
+import { GitHubLogoIcon } from "@radix-ui/react-icons";
+import { Button } from "~/shared/ui/button";
 
 export const Header = () => (
-  <header className="sticky top-0 flex h-16 items-center gap-4 border-b bg-background px-4 md:px-6">
-    <nav className="hidden flex-col gap-6 text-lg font-medium md:flex md:flex-row md:items-center md:gap-5 md:text-sm lg:gap-6">
+  <header className="sticky top-0 flex h-16 items-center justify-between gap-4 border-b bg-background px-4 md:px-6">
+    <nav className="flex flex-col gap-6 text-lg font-medium md:flex-row md:items-center md:gap-5 md:text-sm lg:gap-6">
       <Link
         href="#"
         className="flex items-center gap-2 text-lg font-semibold md:text-base"
       >
-        <PanelBottom className="h-6 w-6" />
+        <PanelBottom className="h-6 w-6" /> Table
         <span className="sr-only">Test Task</span>
       </Link>
     </nav>
+    <div className="flex items-center gap-3">
+      <Button variant="outline" size="icon" asChild>
+        <Link href="https://github.com/velenyx/systeme.io-task">
+          <GitHubLogoIcon className="h-[1.2rem] w-[1.2rem]" />
+        </Link>
+      </Button>
+      <ThemeToggle />
+    </div>
   </header>
 );
