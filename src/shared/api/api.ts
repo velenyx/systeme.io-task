e7675bc3573dceb2974fd056f18ae7a608a3524c
Sivritkin Dmitriy <dmitriysivritkin@gmail.com>
2024-06-07 21:01:54 +0300

    test: :white_check_mark: test fix build errors

--- src/shared/api/api.ts
+++ src/shared/api/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const $api = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
   validateStatus(status) {
     return status >= 200 && status < 300;
   },
